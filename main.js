73ca4b1bdece92ccd88b470fa7e74d20ca13dad3
Amanda Slayton <alslayton86@gmail.com>
2018-03-29 19:58:47 -0500

    added and tested XML request

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Why, hello there.");
+//console.log("Why, hello there.");
+
+//XML HTTP Request on Page Load:
+function ifFileLoads(){
+  console.log("It's alive!!!!")
+};
+
+function ifFails(){
+  console.log("Mistakes have been made.");
+};
+
+//When the JS is called, run this first: 
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", ifFileLoads);
+  myRequest.addEventListener("error", ifFails);
+  myRequest.open("GET", "planet.json");
+  myRequest.send();
+};
+
+startApplication();
